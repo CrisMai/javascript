678ce6dcf8160893b4627a56d33576cf242575a7
CrisMai <crismai.dev@gmail.com>
2022-09-26 16:47:02 -0300

    Validação e cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,37 @@
-
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "CrisMai Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if (peso <= 0 || peso >= 1000) {
     console.log("Peso inválido!");
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido!";
-}
+  }
 
-if (altura <= 0 || altura >= 3.00) {
+  if (altura <= 0 || altura >= 3.0) {
     console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida!";
+  }
+
+  if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
-
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso /(altura * altura);
-    tdImc.textContent = imc;
-}
-
-
-
