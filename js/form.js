4118c908da6e9c9e6ff3c0eb943133f2e4da5a99
CrisMai <crismai.dev@gmail.com>
2022-09-27 15:50:59 -0300

    Organizando o código em vários arquivos

--- js/form.js
+++ js/form.js
@@ -1,42 +1,3 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "CrisMai Nutricionista";
-
-var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
-
-for (var i = 0; i < pacientes.length; i++) {
-  var paciente = pacientes[i];
-
-  var tdPeso = paciente.querySelector(".info-peso");
-  var peso = tdPeso.textContent;
-
-  var tdAltura = paciente.querySelector(".info-altura");
-  var altura = tdAltura.textContent;
-
-  var tdImc = paciente.querySelector(".info-imc");
-
-  var pesoEhValido = true;
-  var alturaEhValida = true;
-
-  if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
-    paciente.classList.add("paciente-invalido");
-  }
-
-  if (altura <= 0 || altura >= 3.0) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-    paciente.classList.add("paciente-invalido");
-  }
-
-  if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
-  }
-}
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) { //função anônima
@@ -60,14 +21,15 @@ botaoAdicionar.addEventListener("click", function(event) { //função anônima
   pesoTd.textContent = peso;
   alturaTd.textContent = altura;
   gorduraTd.textContent = gordura;
+  imcTd.textContent = calculaImc(peso, altura);
   
   pacienteTr.appendChild(nomeTd);
   pacienteTr.appendChild(pesoTd);
   pacienteTr.appendChild(alturaTd);
   pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
 
   var tabela = document.querySelector("#tabela-pacientes");
   tabela.appendChild(pacienteTr);
 
 })
-
